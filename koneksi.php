7b2edfcf2835bd8feb16dfc4bfbaaf575cc43ce9
nurnailibalqis-sys <nurnaili.balqis@gmail.com>
2026-01-11 19:26:10 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "warugocrud";
+$db   = "rancangpraktik4";
 $mysqli = new mysqli($host, $user, $pass, $db);
 if ($mysqli->connect_errno) {
     die("Koneksi gagal: " . $mysqli->connect_error);
